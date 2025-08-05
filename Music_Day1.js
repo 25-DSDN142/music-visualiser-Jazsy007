00ee0c4dfc56602027f7baa4d02c88ef5f2ef049
Jazsy007 <molloyjasm@myvuw.ac.nz>
2025-08-05 12:08:11 +1200

    Day 1

---
+++ Music_Day1.js
@@ -0,0 +1,44 @@
+// const canvasWidth = 1280;
+// const canvasHeight = 720;
+
+let vocalellipse;
+let drumellipse;
+let bassellipse;
+let otherellipse;
+
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+
+  background(20)
+  textFont('Impact'); // please use CSS safe fonts
+  rectMode(CENTER)
+  textSize(10);
+ 
+  vocalellipse = map(vocal, 0, 100, 30, 200)
+  drumellipse = map(drum, 0, 100, 30, 200)
+  bassellipse = map(bass, 0, 100, 30, 200)
+  otherellipse = map(other, 0, 100, 30, 200)
+
+
+// Vocal Ellipse
+fill(120, 255, 255)
+ellipse(376, 240, vocalellipse)
+
+// Drum Ellipse
+fill(255, 117, 223)
+ellipse(902, 240, drumellipse)
+
+// Bass Ellipse
+fill(0, 54, 214)
+ellipse(376, 480, bassellipse)
+
+// Other Ellipse
+fill(171, 3, 90)
+ellipse(902, 480, otherellipse)
+
+fill(255)
+ // display "words"
+   textAlign(CENTER);
+   textSize(vocal);
+   text(words, 640, 360);
+}
